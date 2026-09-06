3f1e57b41562f22acdca500d029c5771a731568e
anirakapp <anirakconstruccion@gmail.com>
2026-09-05 21:14:03 -0300

    Add files via upload

---
+++ app/panel/page.tsx
@@ -0,0 +1,6 @@
+import type { ReactElement } from "react";
+import HomeView from "./components/HomeView";
+
+export default function Page(): ReactElement {
+  return <HomeView ciudad="Rosario" />;
+}
